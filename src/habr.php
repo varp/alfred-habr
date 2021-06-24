76dabf44a8b1b4678539bcff57403a7531a0d7d4
Vardan Pogosian <vardan.pogosyan@gmail.com>
2021-06-24 21:42:41 +0300

    refactor

--- src/habr.php
+++ src/habr.php
@@ -6,11 +6,44 @@ declare(strict_types=1);
 const ENV_ALFRED_HABR_FEED = 'ALFRED_HABR_FEED';
 
 const FEED_BEST = 'best';
-const FEED_ALL = 'all';
+const FEED_EVERYTHING = 'all';
 
 const FEED_URLS = [
-    FEED_BEST => 'https://habr.com/ru/rss/best/daily/?fl=ru%2Cen',
-    FEED_ALL  => 'https://habr.com/ru/rss/all/all/?fl=ru',
+    FEED_BEST       => 'https://habr.com/ru/rss/best/%s/?fl=ru&limit=100&with_hubs=true:&with_tags=true:',
+    FEED_EVERYTHING => 'https://habr.com/ru/rss/all/%s/?fl=ru&limit=100&with_hubs=true:&with_tags=true:',
+];
+
+const FEED_BEST_PERIOD_DAILY = 'daily';
+const FEED_BEST_PERIOD_WEEKLY = 'weekly';
+const FEED_BEST_PERIOD_MONTHLY = 'monthly';
+const FEED_BEST_PERIOD_YEARLY = 'yearly';
+const FEED_BEST_PERIOD_ALL_TIME = 'alltime';
+
+const FEED_EVERYTHING_PERIOD_ALL = 'all';
+const FEED_EVERYTHING_PERIOD_TOP0 = 'top0';
+const FEED_EVERYTHING_PERIOD_TOP10 = 'top10';
+const FEED_EVERYTHING_PERIOD_TOP25 = 'top25';
+const FEED_EVERYTHING_PERIOD_TOP50 = 'top50';
+const FEED_EVERYTHING_PERIOD_TOP100 = 'top100';
+
+const FEED_PERIODS = [
+    FEED_BEST_PERIOD_DAILY,
+    FEED_BEST_PERIOD_WEEKLY,
+    FEED_BEST_PERIOD_MONTHLY,
+    FEED_BEST_PERIOD_YEARLY,
+    FEED_BEST_PERIOD_ALL_TIME,
+
+    FEED_EVERYTHING_PERIOD_ALL,
+    FEED_EVERYTHING_PERIOD_TOP0,
+    FEED_EVERYTHING_PERIOD_TOP10,
+    FEED_EVERYTHING_PERIOD_TOP25,
+    FEED_EVERYTHING_PERIOD_TOP50,
+    FEED_EVERYTHING_PERIOD_TOP100,
+];
+
+const FEED_DEFAULT_PERIODS = [
+    FEED_BEST       => FEED_BEST_PERIOD_DAILY,
+    FEED_EVERYTHING => FEED_EVERYTHING_PERIOD_ALL,
 ];
 
 /**
@@ -42,12 +75,12 @@ function parseFeed(string $url): array
         $result['autocomplete'] = $title;
         $result['quicklookurl'] = $link;
         $result['mods'] = [
-            "alt" => [
-                "arg" => $title
+            "alt"     => [
+                "arg" => $title,
 
             ],
             "control" => [
-                "arg" => $guid
+                "arg" => $guid,
             ],
         ];
 
@@ -61,17 +94,28 @@ function getURL(): string
 {
     global $argv;
 
-    $feedName = !empty($argv[1]) ? $argv[1] : getenv(ENV_ALFRED_HABR_FEED);
+    $feedAndPeriod = !empty($argv[1]) ? $argv[1] : getenv(ENV_ALFRED_HABR_FEED);
 
-    if (!empty($feedName) && array_key_exists($feedName, FEED_URLS)) {
-        return FEED_URLS[$feedName];
+    if (empty($feedAndPeriod)) {
+        return sprintf(FEED_URLS[FEED_BEST], FEED_DEFAULT_PERIODS[FEED_BEST]);
     }
 
-    return FEED_URLS[FEED_BEST];
+    [$feedName, $feedPeriod] = explode('-', $feedAndPeriod);
+
+    $feedPeriod = empty($feedPeriod) ? FEED_DEFAULT_PERIODS[$feedName] : $feedPeriod;
+
+    if (
+        !empty($feedName)
+        && array_key_exists($feedName, FEED_URLS)
+        && !empty($feedPeriod)
+        && in_array($feedPeriod, FEED_PERIODS, true)
+    ) {
+        return sprintf(FEED_URLS[$feedName], $feedPeriod);
+    }
 }
 
 $url = getURL();
 $alfredJson = parseFeed($url);
-$alfredJson['variables'][ENV_ALFRED_HABR_FEED] = array_flip(FEED_URLS)[$url];
+$alfredJson['variables'][ENV_ALFRED_HABR_FEED] = $url;
 
 echo json_encode($alfredJson);
